aaef0d7b59c9050bf916fd6e923b49514a395fe3
JeyHicks <jeyhicks@gmail.com>
2017-01-06 19:13:54 +0100

    AI Controllers now can find the player pawn

--- Source/BattleTank/Private/TankAIController.cpp
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -8,14 +8,14 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller not possessing a tank"));
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller can't find player tank"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AI Controller possesing: %s"), *(ControlledTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller found player: %s"), *(PlayerTank->GetName()));
 	}
 }
 
@@ -24,6 +24,11 @@ ATank* ATankAIController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
-
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
 
 
