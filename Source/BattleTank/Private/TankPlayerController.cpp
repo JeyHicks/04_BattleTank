f8ef166928de4d39dc4079c3d669224d6f6adc33
JeyHicks <jeyhicks@gmail.com>
2017-01-07 11:38:46 +0100

    Added Ticking to tank player controller. Added basic framework for aiming system

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -19,9 +19,28 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	//line trace through crosshair
+
+	//if trace hits something
+		//aim at hit location
+
+}
 
--- Source/BattleTank/Public/TankPlayerController.h
+++ Source/BattleTank/Public/TankPlayerController.h
@@ -18,7 +18,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 	ATank* GetControlledTank() const;
 
-	void BeginPlay() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
 	
 	
+	void AimTowardsCrosshair();
 };
