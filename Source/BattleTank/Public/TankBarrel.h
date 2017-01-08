26a7c19189029f506b33874258f63c7d2f2ae3a3
JeyHicks <jeyhicks@gmail.com>
2017-01-08 10:38:16 +0100

    Added new Barrel class to tank Blueprint. Added new variables to the barrel class. Set some good defaults

--- Source/BattleTank/Public/TankBarrel.h
+++ Source/BattleTank/Public/TankBarrel.h
@@ -6,9 +6,9 @@
 #include "TankBarrel.generated.h"
 
 /**
- * 
+ * Holds Parameters for barrels properties and elevate method
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision")) // Makes it so you can add this via blueprint and hides collsion properties.
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -17,5 +17,13 @@ public:
 	void Elevate(float DegreesPerSecond);
 
 private:
+	UPROPERTY(EditAnywhere, Category = BarrelSetup)
+		float MaxDegreesPerSecond = 25; 
 	
+	UPROPERTY(EditAnywhere, Category = BarrelSetup)
+		float MaxElevationDegrees = 20;
+
+	UPROPERTY(EditAnywhere, Category = BarrelSetup)
+		float MinElevationDegrees = 0; //sensible default. Would like it to go to -10 just while facing forwards.
+
 };
